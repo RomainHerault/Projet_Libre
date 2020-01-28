634080a774e7a6d8bc7308b0238fd1523b9175cc
21506969t <bobap@hotmail.fr>
2020-01-28 17:06:19 +0100

    update of extract class

--- asteroids-master/src/neural_network/carac_extract.py
+++ asteroids-master/src/neural_network/carac_extract.py
@@ -3,7 +3,7 @@ class Extract:
         pass
 
     def display_rock_position(self,rock_list):
+        print('number of rocks',len(rock_list))
         for rock in rock_list:
-
             print('position',rock.position.x,rock.position.y)
             print('bouding box',rock.boundingRect)
