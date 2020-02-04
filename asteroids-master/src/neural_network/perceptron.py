89471d918f83517cf62a0e030c48bb4e116e139f
21506969t <bobap@hotmail.fr>
2020-02-04 14:38:16 +0100

    perceptron third version

--- asteroids-master/src/neural_network/perceptron.py
+++ asteroids-master/src/neural_network/perceptron.py
@@ -3,24 +3,26 @@ import keras
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.optimizers import Adam
 from keras.utils import to_categorical
 from keras.models import load_model
-
+import numpy as np
+from carac_extract import load_data
 
 # Configuration options
 num_classes = 4
 
 # Load the data
-(X_train, Y_train) = ...
+(X_train, Y_train) = load_data(r'C:\Users\21506969t\PycharmProjects\Projet_Libre\asteroids-master\src\SavedData\dataset_04-02-2020_14-22-14')
 
 # Create the model
 model = Sequential()
 model.add(Dense(50, input_shape=(50, 6), activation='relu'))
-model.add(Dense(6, activation='relu'))
+model.add(Dense(25, activation='relu'))
 model.add(Dense(num_classes, activation='softmax'))
 
 #optimizer
-opt = keras.optimizers.SGD(learning_rate=0.01, momentum=0.0, nesterov=False)
+opt = keras.optimizers.SGD(lr=0.01, momentum=0.0, nesterov=False)
 
 # Configure the model and start training
 model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
